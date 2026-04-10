74cd6631189ce5c90457786126d8091c44deb445
Hamda Osman <hamdaosman099@gmail.com>
2026-04-10 13:02:27 +0200

    try2

---
+++ Class skeletons/Product.java
@@ -0,0 +1,19 @@
+import java.util.List;
+public class Product {
+    private String name;
+    private String category;
+    private int estimatedLifespan;
+    private List<Material> material;
+    
+
+    public void addMaterial(Material material){
+        
+    }
+    
+    public calcutateImpact(){
+        return 0.0;
+    }
+    public String getRecyclingGuidance(){
+        return "";
+    }   
+}
