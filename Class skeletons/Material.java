75a174a8a3c1ea6e5385a452c75d0ad01f3dfb32
Hamda Osman <hamdaosman099@gmail.com>
2026-04-10 13:15:46 +0200

    try 3

---
+++ Class skeletons/Material.java
@@ -0,0 +1,10 @@
+public class Material{
+    private String name;
+    private double impactValue;
+    private String recyclingCategory;
+    private String recyclingInstruction;
+
+    public String getRecyclingGuidance(){
+
+    }
+}
