b3085cc9f2e7a0c13fb556d2a072f2ff27845046
angelatrapara <dinmail@gmail.com>
2026-05-07 15:26:39 +0200

    improved menu structure and input validation

--- src/main/java/presentation/Menu.java
+++ src/main/java/presentation/Menu.java
@@ -10,25 +10,36 @@ public class Menu {
         this.scanner = scanner;
     }
  
-    public void showMenu() {
+public void showMenu() {
+        System.out.println();
         System.out.println("HÅLLBART PRODUKTSYSTEM");
-        System.out.println("1. Skapa produkt");
-        System.out.println("2. Lista produkter");
-        System.out.println("3. Visa produktdetaljer");
-        System.out.println("4. Beräkna miljöpåverkan");
-        System.out.println("5. Visa återvinningsråd");
-        System.out.println("6. Skapa material");
-        System.out.println("7. Lista material");
-        System.out.println("8. Lägg till material i produkt");
-        System.out.println("9. Byt beräkningsstrategi");
-        System.out.println("10. Avsluta");
-        System.out.print("Välj ett alternativ: ");
+        System.out.println("1) Skapa ny produkt");
+        System.out.println("2) Visa alla produkter");
+        System.out.println("3) Visa produktinformation");
+        System.out.println("4) Räkna ut miljöpåverkan");
+        System.out.println("5) Visa återvinningsinfo");
+        System.out.println("6) Lägg till material");
+        System.out.println("7) Visa material");
+        System.out.println("8) Koppla material till produkt");
+        System.out.println("9) Byt beräkningsmetod");
+        System.out.println("10) Avsluta");
+        System.out.print("Ange ditt val: ");
     }
  
-    public int readChoice() {
-        int choice = scanner.nextInt();
-        scanner.nextLine();
+public int readChoice() {
+        int choice;
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+                scanner.nextLine(); 
+                break;
+            } else {
+                System.out.print("Felaktig input, försök igen: ");
+                scanner.next(); 
+            }
+        }
+
         return choice;
     }
 }
- 
