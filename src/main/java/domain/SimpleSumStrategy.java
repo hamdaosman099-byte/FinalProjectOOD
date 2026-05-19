11ef4f248fbd53dceaf50a3034630db1b21e9f4b
angelatrapara <dinmail@gmail.com>
2026-05-19 13:37:48 +0200

    Refactoring SimpleSumStrategy + refactoring summary (week 9)

--- src/main/java/domain/SimpleSumStrategy.java
+++ src/main/java/domain/SimpleSumStrategy.java
@@ -1,10 +1,17 @@
 package domain;
 
+import java.util.List;
+
 public class SimpleSumStrategy implements ImpactStrategy {
+
     @Override
     public double calculateImpact(Product product) {
+        return sumMaterialImpact(product.getMaterials());
+    }
+
+    private double sumMaterialImpact(List<Material> materials) {
         double total = 0;
-        for (Material m : product.getMaterials()) {
+        for (Material m : materials) {
             total += m.getImpactValue();
         }
         return total;
